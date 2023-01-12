879c3f6ee730b529d67cd75223d5c1baab0cb320
Perriesha <pmbyrd@gmail.com>
2023-01-12 12:04:52 -0800

    added favorite icon

--- js/stories.js
+++ js/stories.js
@@ -18,21 +18,21 @@ async function getAndShowStoriesOnStart() {
  *
  * Returns the markup for the story.
  */
-
+// *this function was included in the original code
 function generateStoryMarkup(story) {
   // console.debug("generateStoryMarkup", story);
-
+  // const star = <i class="fa-regular fa-star"></i>
   const hostName = story.getHostName();
   return $(`
       <li id="${story.storyId}">
+        <a href="#" class="star"><span class="star"> <i class="far fa-star"></i> </span></a>
         <a href="${story.url}" target="a_blank" class="story-link">
           ${story.title}
         </a>
         <small class="story-hostname">(${hostName})</small>
         <small class="story-author">by ${story.author}</small>
         <small class="story-user">posted by ${story.username}</small>
-      </li>
-    `);
+      </li>`);
 }
 
 /** Gets list of stories from server, generates their HTML, and puts on page. */
@@ -94,58 +94,14 @@ async function handleSubmitStory(evt) {
 // $newStoryForm.on("submit", handleSubmitStory)
 $newStoryForm.on("submit", handleSubmitStory);
 
-// todo make a function to add and dislike a story
-// * this function will be called when a user clicks on the favorite icon
-// *for all stories add a favorite icon
-async function handleFavoriteStory(evt) {
-  // get the element where where each story is being displayed
-  // print each story to the console
-}
-// for each story map over the story list and print the story to the console
-async function displayLike() {
-  console.log("displaying like")
-  // get the story id
-  let storyId = $(this).closest("li").attr("id")
-  // get the story from the story list
-  let story = storyList.stories.find(story => story.storyId === storyId)
-  // get the story's like count
-  let likeCount = story.likes
-  // get the story's dislike count
-  let dislikeCount = story.dislikes
-  // get the story's favorite count
-  let favoriteCount = story.favorites
-  // get the story's user
-  let user = story.username
-  // get the story's author
-  let author = story.author
-  // get the story's title
-  let title = story.title
-  // get the story's url
-  let url = story.url
-  // get the story's host name
-  let hostName = story.getHostName()
-  // get the story's user
-  let username = story.username
-  // get the story's date
-  let date = story.createdAt
-  // get the story's update date
-  let updateDate = story.updatedAt
-  // get the story's id
-  let id = story.storyId
-  // get the story's favorite status
-  let favoriteStatus = story.isFavorite
-
-  }
-  // todo make a function isFavorite()
+// todo isFavorite() function to add and dislike a story
+// * this function will be called when a user clicks on the like link
+async function isFavorite() {
+  //if a user clicks the favorite link
+  // check if the story is already a favorite
+  // if it is a favorite remove it from the favorites list
+  // if it is not a favorite add it to the favorites list
   
-  // 
+}
 
-// if the document is ready console.log the story list
-$(document).ready(function () {
-
-  let $allStoriesList = $(".all-stories-list");
-  console.log($allStoriesList)
-  console.table($allStoriesList)
-  console.log(storyList)
-  console.log($(".story-link"))
-})
+  
